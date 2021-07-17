bd9d9e68b091004c8dbeebe595de3ee4942be1ff
Yasser Tahiri <yasserth19@gmail.com>
2021-07-17 01:21:35 +0100

    ✨Fix the files :rocket:

--- instagram/proxy_service.py
+++ instagram/proxy_service.py
@@ -1,3 +1,4 @@
 Config = {
-    "proxy_server" : "input your proxy server"  #213.168.210.76 sample proxy server to prevent instagram's ip ban
+    # 213.168.210.76 sample proxy server to prevent instagram's ip ban
+    "proxy_server": "input your proxy server"
 }
